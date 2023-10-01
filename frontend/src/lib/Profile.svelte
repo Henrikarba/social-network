329443e0fa2a8f7c7b2bee695f825625fda70f7a
Henri <henrikarba@gmail.com>
2023-10-01 16:58:44 +0300

    profile posts

--- frontend/src/lib/Profile.svelte
+++ frontend/src/lib/Profile.svelte
@@ -1,123 +1,161 @@
 <script>
-	import { slide } from 'svelte/transition'
-	// Svelte
-	import { createEventDispatcher } from 'svelte'
-	const dispatch = createEventDispatcher()
-	//
-	import { formatDateTime } from '../utils.js'
-	import { socket } from '../ws'
+  import { slide } from "svelte/transition";
+  // Svelte
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  //
+  import { formatDateTime } from "../utils.js";
+  import { socket } from "../ws";
+  import { postsStore } from "../stores/post";
 
-	// Icons
-	import FaBirthdayCake from 'svelte-icons/fa/FaBirthdayCake.svelte'
+  // Icons
+  import FaBirthdayCake from "svelte-icons/fa/FaBirthdayCake.svelte";
 
-	// Stores
-	import { currentUser, currentUserFollowing, currentUserGroups, currentUserFollowers } from '../stores/user'
+  // Stores
+  import {
+    currentUser,
+    currentUserFollowing,
+    currentUserGroups,
+    currentUserFollowers,
+  } from "../stores/user";
 
-	let user = $currentUser
-	let groups = $currentUserGroups
-	$: followers = $currentUserFollowers ? $currentUserFollowers.filter((item) => item.status != 'pending') : []
-	$: following = $currentUserFollowing ? $currentUserFollowing.filter((item) => item.status != 'pending') : []
+  let user = $currentUser;
+  let groups = $currentUserGroups;
+  $: followers = $currentUserFollowers
+    ? $currentUserFollowers.filter((item) => item.status != "pending")
+    : [];
+  $: following = $currentUserFollowing
+    ? $currentUserFollowing.filter((item) => item.status != "pending")
+    : [];
 
-	// Birthday
-	let birthday = formatDateTime(user.date_of_birth)
+  // Birthday
+  let birthday = formatDateTime(user.date_of_birth);
 
-	const hackerman = new URL('../assets/robot.jpg', import.meta.url).href
+  const hackerman = new URL("../assets/robot.jpg", import.meta.url).href;
 
-	function togglePrivacy() {
-		user.privacy == 1 ? (user.privacy = 0) : (user.privacy = 1)
-		const data = {
-			action: 'toggle_privacy',
-		}
-		socket.send(JSON.stringify(data))
-	}
-	$: console.log(followers, following)
+  function togglePrivacy() {
+    user.privacy == 1 ? (user.privacy = 0) : (user.privacy = 1);
+    const data = {
+      action: "toggle_privacy",
+    };
+    socket.send(JSON.stringify(data));
+  }
+  $: console.log(followers, following);
+  $: userPosts = $postsStore.filter((item) => item.user_id == $currentUser.id);
 </script>
 
 <div class="mt-10 flex flex-col items-center">
-	<div class="flex">
-		<h2 class="text-4xl">Welcome back {user.first_name} {user.last_name}!</h2>
-		<img class="w-10" src="http://localhost:80/images/{user.avatar}" alt="" />
-	</div>
-	<div class="flex items-center">
-		<div class="w-10 mr-4">
-			<FaBirthdayCake />
-		</div>
-		<div class="text-xl">
-			{birthday}
-		</div>
-	</div>
-	<div class="p-4 mt-2 border-r-emerald-700 border-8 rounded-xl border-t-red-500 border-l-cyan-600 border-b-orange-400">
-		<h2>Congratz, your birthday is coming soon. Or later. Depends on when you were born</h2>
-	</div>
-	<div class="mt-2">
-		{#if user?.about_me}
-			<h2>About me:</h2>
-			{user.about_me}
-		{/if}
-	</div>
-	<div class="flex gap-2">
-		<div class="mt-2 border-2 border-zinc-400 p-4">
-			{#if groups && groups.length > 0}
-				<h2 class="border-b-2">My groups</h2>
-				{#each groups.filter((item) => item.status == 'joined') as group, index (group.id)}
-					<h2>{group.title}</h2>
-				{/each}
-			{:else}
-				<h2>You aren't in any groups :(</h2>
-			{/if}
-		</div>
-		<div class="mt-2 border-2 border-zinc-400 p-4">
-			{#if followers && followers.length > 0}
-				<h2 class="border-b-2 font-bold">My followers</h2>
-				{#each followers as follower, index (follower.id)}
-					<h2 on:click={() => dispatch('user', follower.id)} class="text-orange-800 cursor-pointer font-extrabold">
-						{follower.first_name}
-						{follower.last_name}
-					</h2>
-				{/each}
-			{:else}
-				<h2 class="border-b-2 font-bold">My impressive list of no one who follows me:</h2>
-			{/if}
-		</div>
-		<div class="mt-2 border-2 border-zinc-400 p-4">
-			{#if following}
-				<h2 class="border-b-2 font-bold">I follow these interesting guys:</h2>
-				{#each following as follower, index (follower.id)}
-					<h2 on:click={() => dispatch('user', follower.id)} class="text-orange-800 cursor-pointer font-extrabold">
-						{follower.first_name}
-						{follower.last_name}
-					</h2>
-				{/each}
-			{:else}
-				<h2 class="border-b-2 font-bold">no one follows me</h2>
-			{/if}
-		</div>
-	</div>
-	<div class="mt-6 flex flex-col justify-center items-center">
-		<h2>USE VPN?! Toggle privacy mode</h2>
-		<div class="flex items-center">
-			<input
-				on:click={() => {
-					togglePrivacy()
-				}}
-				bind:checked={user.privacy}
-				id="checked-checkbox"
-				type="checkbox"
-				value=""
-				class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-			/>
-		</div>
-		{#if user.privacy == 0}
-			<div class="flex items-center">
-				<h2>you are currently not using vpn and tor and blockchain and gfx card to hide your identity</h2>
-			</div>
-		{:else}
-			<div>
-				<h2 class="text-4xl font-extrabold text-green-500">STATUS:</h2>
-				<img class="w-6/12" src={hackerman} alt="you are private citizen!" />
-			</div>
-		{/if}
-	</div>
+  <div class="flex">
+    <h2 class="text-4xl">Welcome back {user.first_name} {user.last_name}!</h2>
+    <img class="w-10" src="http://localhost:80/images/{user.avatar}" alt="" />
+  </div>
+  <div class="flex items-center">
+    <div class="w-10 mr-4">
+      <FaBirthdayCake />
+    </div>
+    <div class="text-xl">
+      {birthday}
+    </div>
+  </div>
+  <div
+    class="p-4 mt-2 border-r-emerald-700 border-8 rounded-xl border-t-red-500 border-l-cyan-600 border-b-orange-400"
+  >
+    <h2>
+      Congratz, your birthday is coming soon. Or later. Depends on when you were
+      born
+    </h2>
+  </div>
+  <div class="mt-2">
+    {#if user?.about_me}
+      <h2>About me:</h2>
+      {user.about_me}
+    {/if}
+  </div>
+  <div class="flex gap-2">
+    <div class="mt-2 border-2 border-zinc-400 p-4">
+      {#if groups && groups.length > 0}
+        <h2 class="border-b-2">My groups</h2>
+        {#each groups.filter((item) => item.status == "joined") as group, index (group.id)}
+          <h2>{group.title}</h2>
+        {/each}
+      {:else}
+        <h2>You aren't in any groups :(</h2>
+      {/if}
+    </div>
+    <div class="mt-2 border-2 border-zinc-400 p-4">
+      {#if followers && followers.length > 0}
+        <h2 class="border-b-2 font-bold">My followers</h2>
+        {#each followers as follower, index (follower.id)}
+          <h2
+            on:click={() => dispatch("user", follower.id)}
+            class="text-orange-800 cursor-pointer font-extrabold"
+          >
+            {follower.first_name}
+            {follower.last_name}
+          </h2>
+        {/each}
+      {:else}
+        <h2 class="border-b-2 font-bold">
+          My impressive list of no one who follows me:
+        </h2>
+      {/if}
+    </div>
+    <div class="mt-2 border-2 border-zinc-400 p-4">
+      {#if following}
+        <h2 class="border-b-2 font-bold">I follow these interesting guys:</h2>
+        {#each following as follower, index (follower.id)}
+          <h2
+            on:click={() => dispatch("user", follower.id)}
+            class="text-orange-800 cursor-pointer font-extrabold"
+          >
+            {follower.first_name}
+            {follower.last_name}
+          </h2>
+        {/each}
+      {:else}
+        <h2 class="border-b-2 font-bold">no one follows me</h2>
+      {/if}
+    </div>
+  </div>
+  {#if userPosts && userPosts.length > 0}
+    <h2 class="font-bold text-xl">Posts created by me:</h2>
+    {#each userPosts as post}
+      <h2
+        class="text-primary hover:cursor-pointer"
+        on:click={() => dispatch("singlePost", post.post_id)}
+      >
+        {post.title}
+      </h2>
+    {/each}
+  {/if}
+  <div class="mt-6 flex flex-col justify-center items-center">
+    <h2>USE VPN?! Toggle privacy mode</h2>
+    <div class="flex items-center">
+      <input
+        on:click={() => {
+          togglePrivacy();
+        }}
+        bind:checked={user.privacy}
+        id="checked-checkbox"
+        type="checkbox"
+        value=""
+        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+      />
+    </div>
+    {#if user.privacy == 0}
+      <div class="flex items-center">
+        <h2>
+          you are currently not using vpn and tor and blockchain and gfx card to
+          hide your identity
+        </h2>
+      </div>
+    {:else}
+      <div>
+        <h2 class="text-4xl font-extrabold text-green-500">STATUS:</h2>
+        <img class="w-6/12" src={hackerman} alt="you are private citizen!" />
+      </div>
+    {/if}
+  </div>
 </div>
 
 <style>
--- frontend/src/lib/ViewProfile.svelte
+++ frontend/src/lib/ViewProfile.svelte
@@ -1,167 +1,205 @@
 <script>
-	import { currentUser } from '../stores/user'
-	import { socket } from '../ws'
-	// Svelte
-	import { createEventDispatcher } from 'svelte'
-	const dispatch = createEventDispatcher()
-	import { currentUserFollowing, currentUserGroups } from '../stores/user'
-	import { postsStore } from '../stores/post'
-	import { formatDateTime } from '../utils'
-	export let profile
+  import { currentUser } from "../stores/user";
+  import { socket } from "../ws";
+  // Svelte
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  import { currentUserFollowing, currentUserGroups } from "../stores/user";
+  import { postsStore } from "../stores/post";
+  import { formatDateTime } from "../utils";
+  export let profile;
 
-	function makeFollowRequest(id) {
-		if (!$currentUserFollowing) {
-			$currentUserFollowing = []
-		}
+  function makeFollowRequest(id) {
+    if (!$currentUserFollowing) {
+      $currentUserFollowing = [];
+    }
 
-		if (!followers) {
-			followers = []
-		}
+    if (!followers) {
+      followers = [];
+    }
 
-		if (!privateProfile) {
-			$currentUserFollowing = [...$currentUserFollowing, { ...profile.user, status: 'accepted' }]
-			followers = [...followers, $currentUser]
-		} else if (privateProfile) {
-			$currentUserFollowing = [...$currentUserFollowing, { ...profile.user, status: 'pending' }]
-		}
-		const data = {
-			action: 'follow_request',
-			data: {
-				id: parseInt(id),
-			},
-		}
-		console.log('SENDING....')
-		socket.send(JSON.stringify(data))
-	}
+    if (!privateProfile) {
+      $currentUserFollowing = [
+        ...$currentUserFollowing,
+        { ...profile.user, status: "accepted" },
+      ];
+      followers = [...followers, $currentUser];
+    } else if (privateProfile) {
+      $currentUserFollowing = [
+        ...$currentUserFollowing,
+        { ...profile.user, status: "pending" },
+      ];
+    }
+    const data = {
+      action: "follow_request",
+      data: {
+        id: parseInt(id),
+      },
+    };
+    console.log("SENDING....");
+    socket.send(JSON.stringify(data));
+  }
 
-	// Determine if the current user is following the profile
-	let isFollowing = ''
-	$: if ($currentUserFollowing) {
-		const follower = $currentUserFollowing.find((follower) => follower.id === profile.user.id)
+  // Determine if the current user is following the profile
+  let isFollowing = "";
+  $: if ($currentUserFollowing) {
+    const follower = $currentUserFollowing.find(
+      (follower) => follower.id === profile.user.id
+    );
 
-		if (follower) {
-			isFollowing = follower.status === 'accepted' ? 'accepted' : 'pending'
-		}
-	}
-	$: privateProfile = !profile.user?.email
-	let followers
-	let following
-	if (profile?.followers) {
-		followers = profile.followers
-	}
-	if (profile?.following) {
-		following = profile.following
-	}
-	let inviteableGroups
-	$: joinedGroups = $currentUserGroups ? $currentUserGroups.filter((group) => group.status == 'joined') : []
-	$: if (joinedGroups && joinedGroups.length > 0) {
-		inviteableGroups = joinedGroups.filter((item) => {
-			return !item.member_ids.some((elem) => elem == profile.user.id)
-		})
-	}
-	$: console.log(inviteableGroups)
-	let selectedGroup
-	$: invites = []
-	function inviteToGroup() {
-		const data = {
-			action: 'group_join_invite',
-			data: {
-				user_id: profile.user.id,
-				id: parseInt(selectedGroup),
-			},
-		}
-		$currentUserGroups = $currentUserGroups.map((item) => {
-			if (item.id == selectedGroup) {
-				item.member_ids = [...item.member_ids, profile.user.id]
-			}
-			return item
-		})
-		socket.send(JSON.stringify(data))
-		invites = [
-			...invites,
-			`Invited ${profile.user.first_name} ${profile.user.last_name} to ${
-				inviteableGroups.find((item) => item.id == selectedGroup).title
-			}`,
-		]
-	}
-	$: console.log(selectedGroup)
-	$: userPosts = $postsStore.filter((item) => item.user_id == profile.user.id)
+    if (follower) {
+      isFollowing = follower.status === "accepted" ? "accepted" : "pending";
+    }
+  }
+  $: privateProfile = !profile.user?.email;
+  let followers;
+  let following;
+  if (profile?.followers) {
+    followers = profile.followers;
+  }
+  if (profile?.following) {
+    following = profile.following;
+  }
+  let inviteableGroups;
+  $: joinedGroups = $currentUserGroups
+    ? $currentUserGroups.filter((group) => group.status == "joined")
+    : [];
+  $: if (joinedGroups && joinedGroups.length > 0) {
+    inviteableGroups = joinedGroups.filter((item) => {
+      return !item.member_ids.some((elem) => elem == profile.user.id);
+    });
+  }
+  $: console.log(inviteableGroups);
+  let selectedGroup;
+  $: invites = [];
+  function inviteToGroup() {
+    const data = {
+      action: "group_join_invite",
+      data: {
+        user_id: profile.user.id,
+        id: parseInt(selectedGroup),
+      },
+    };
+    $currentUserGroups = $currentUserGroups.map((item) => {
+      if (item.id == selectedGroup) {
+        item.member_ids = [...item.member_ids, profile.user.id];
+      }
+      return item;
+    });
+    socket.send(JSON.stringify(data));
+    invites = [
+      ...invites,
+      `Invited ${profile.user.first_name} ${profile.user.last_name} to ${
+        inviteableGroups.find((item) => item.id == selectedGroup).title
+      }`,
+    ];
+  }
+  $: console.log(selectedGroup);
+  $: userPosts = $postsStore.filter((item) => item.user_id == profile.user.id);
 </script>
 
 <div class="flex items-center mt-10 border-4 flex-col p-6">
-	<h2>Viewing the profile of:</h2>
-	{#if profile.user?.avatar}
-		<img class="w-16" src="http://localhost:80/images/{profile.user.avatar}" alt="" />
-	{/if}
-	<h2 class="text-4xl font-extrabold">{profile.user.first_name} {profile.user.last_name}</h2>
+  <h2>Viewing the profile of:</h2>
+  {#if profile.user?.avatar}
+    <img
+      class="w-16"
+      src="http://localhost:80/images/{profile.user.avatar}"
+      alt=""
+    />
+  {/if}
+  <h2 class="text-4xl font-extrabold">
+    {profile.user.first_name}
+    {profile.user.last_name}
+  </h2>
 
-	{#if isFollowing == '' || (isFollowing == 'pending' && privateProfile)}
-		<div>
-			<p>This person is using technologies and such to stay private, so no more info for you.</p>
-			<br />
-			<p>You may try following him or her and when they accept, you might see something more interesting</p>
-			<br />
-			<p class="font-mono">no promises though</p>
-		</div>
-	{:else}
-		<h2>{profile.user.email}</h2>
-		{#if profile.user?.about_me}
-			<h2>{profile.user.about_me}</h2>
-		{/if}
-		<h2>Born on {formatDateTime(profile.user.date_of_birth)}</h2>
-	{/if}
-	{#if userPosts && userPosts.length > 0}
-		<h2 class="font-bold text-xl">Posts created by me:</h2>
-		{#each userPosts as post}
-			<h2 class="text-primary hover:cursor-pointer" on:click={() => dispatch('singlePost', post.post_id)}>
-				{post.title}
-			</h2>
-		{/each}
-	{/if}
-	{#if followers && followers.length > 0}
-		<h2 class="mt-4 border-t-4">My followers:</h2>
-		<div class="border-2 border-red-950 p-2 flex gap-2">
-			{#each followers as follower, index (follower.id)}
-				<h2 class="text-orange-500 font-extrabold cursor-pointer" on:click={() => dispatch('user', follower.id)}>
-					{follower.first_name}
-					{follower.last_name}
-				</h2>
-			{/each}
-		</div>
-	{/if}
-	{#if following && following.length > 0}
-		<h2 class="mt-4 border-t-4">I follow:</h2>
-		<div class="border-2 border-red-950 p-2 flex gap-2">
-			{#each following as follower, index (follower.id)}
-				<h2 class="text-orange-500 font-extrabold cursor-pointer" on:click={() => dispatch('user', follower.id)}>
-					{follower.first_name}
-					{follower.last_name}
-				</h2>
-			{/each}
-		</div>
-	{/if}
-	{#if isFollowing == ''}
-		<button on:click={() => makeFollowRequest(profile.user.id)} class="btn"
-			>{privateProfile ? 'Send follow request' : 'Start following'}</button
-		>
-	{:else if isFollowing == 'pending'}
-		<h2 class="p-6 border-2 rounded border-red-500">Follow request sent, waiting to hear back..</h2>
-	{/if}
-	{#if inviteableGroups && inviteableGroups.length > 0}
-		<div class="mt-10">
-			<h2>Invite user to your group(s)</h2>
-			<select name="group" bind:value={selectedGroup}>
-				<option disabled selected value="0">Select</option>
-				{#each inviteableGroups as group, index (group.id)}
-					<option value={group.id}>{group.title}</option>
-				{/each}
-			</select>
-			<button class="btn" on:click={inviteToGroup} disabled={selectedGroup == 0 ? true : false}>Invite user</button>
-		</div>
-	{/if}
-	{#if invites && invites.length > 0}
-		{#each invites as invite}
-			<h2 class="text-primary">{invite}!</h2>
-		{/each}
-	{/if}
+  {#if isFollowing == "" || (isFollowing == "pending" && privateProfile)}
+    <div>
+      <p>
+        This person is using technologies and such to stay private, so no more
+        info for you.
+      </p>
+      <br />
+      <p>
+        You may try following him or her and when they accept, you might see
+        something more interesting
+      </p>
+      <br />
+      <p class="font-mono">no promises though</p>
+    </div>
+  {:else}
+    <h2>{profile.user.email}</h2>
+    {#if profile.user?.about_me}
+      <h2>{profile.user.about_me}</h2>
+    {/if}
+    <h2>Born on {formatDateTime(profile.user.date_of_birth)}</h2>
+  {/if}
+  {#if userPosts && userPosts.length > 0}
+    <h2 class="font-bold text-xl">Posts created by me:</h2>
+    {#each userPosts as post}
+      <h2
+        class="text-primary hover:cursor-pointer"
+        on:click={() => dispatch("singlePost", post.post_id)}
+      >
+        {post.title}
+      </h2>
+    {/each}
+  {/if}
+  {#if followers && followers.length > 0}
+    <h2 class="mt-4 border-t-4">My followers:</h2>
+    <div class="border-2 border-red-950 p-2 flex gap-2">
+      {#each followers as follower, index (follower.id)}
+        <h2
+          class="text-orange-500 font-extrabold cursor-pointer"
+          on:click={() => dispatch("user", follower.id)}
+        >
+          {follower.first_name}
+          {follower.last_name}
+        </h2>
+      {/each}
+    </div>
+  {/if}
+  {#if following && following.length > 0}
+    <h2 class="mt-4 border-t-4">I follow:</h2>
+    <div class="border-2 border-red-950 p-2 flex gap-2">
+      {#each following as follower, index (follower.id)}
+        <h2
+          class="text-orange-500 font-extrabold cursor-pointer"
+          on:click={() => dispatch("user", follower.id)}
+        >
+          {follower.first_name}
+          {follower.last_name}
+        </h2>
+      {/each}
+    </div>
+  {/if}
+  {#if isFollowing == ""}
+    <button on:click={() => makeFollowRequest(profile.user.id)} class="btn"
+      >{privateProfile ? "Send follow request" : "Start following"}</button
+    >
+  {:else if isFollowing == "pending"}
+    <h2 class="p-6 border-2 rounded border-red-500">
+      Follow request sent, waiting to hear back..
+    </h2>
+  {/if}
+  {#if inviteableGroups && inviteableGroups.length > 0}
+    <div class="mt-10">
+      <h2>Invite user to your group(s)</h2>
+      <select name="group" bind:value={selectedGroup}>
+        <option disabled selected value="0">Select</option>
+        {#each inviteableGroups as group, index (group.id)}
+          <option value={group.id}>{group.title}</option>
+        {/each}
+      </select>
+      <button
+        class="btn"
+        on:click={inviteToGroup}
+        disabled={selectedGroup == 0 ? true : false}>Invite user</button
+      >
+    </div>
+  {/if}
+  {#if invites && invites.length > 0}
+    {#each invites as invite}
+      <h2 class="text-primary">{invite}!</h2>
+    {/each}
+  {/if}
 </div>
